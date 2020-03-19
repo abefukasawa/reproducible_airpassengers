5bd6fcdc9da9ce61e5b09d41038bd5fb37c45724
abefukasawa <abefukasawa@gmail.com>
2020-03-18 21:28:32 -0300

    create dickey fuller test function

--- workflow/data_utils.py
+++ workflow/data_utils.py
@@ -1,6 +1,10 @@
 from urllib.request import urlretrieve
 import pandas as pd
 import os
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
 
 def data_loading(url, filename, force_download=False):
     """
@@ -39,3 +43,40 @@ def data_loading(url, filename, force_download=False):
     #assert all(df.columns == ['Passengers'])
     
     return df
+
+
+def test_stationary(timeseries):
+    """
+    Applies the augmented Dickey-Fuller test to a given time series and prints
+    its main statistics.
+    
+    Parameters
+    ----------
+    timeseries [pandas.DataFrame]:
+        DataFrame with time index containing observations in sequence.
+        
+    Returns
+    ---------
+    [] only prints statistics
+    """
+    #Determining Rolling Statistics
+    movingAverage = timeseries.rolling(window = 12).mean()
+    movingStd = timeseries.rolling(window = 12).std()
+    
+    #Plotting Rolling Statistics
+    plt.plot(timeseries, linewidth = 2, label = 'Original')
+    plt.plot(movingAverage, linewidth = 2, label = 'Rolling Mean', color = 'r')
+    plt.plot(movingStd, linewidth = 2, label = 'Rolling Std Dev', color = 'k')
+    plt.legend(loc = 'best')
+    plt.title('Rolling Mean and Standard Deviation')
+    plt.show()
+    
+    #Performing Dickey Fuller Test
+    #Performing Augumented Dickey Fuller Test
+    print('Results of the Dickey Fuller Test')
+    dftest = adfuller(x = timeseries['Passengers'], autolag= 'AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    print(dfoutput)
+    for key,value in dftest[4].items():
+        print('Critical Value ({}) = {}'.format(key,value))
+        
